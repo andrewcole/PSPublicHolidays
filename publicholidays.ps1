e106e8cf6b0030c20dfe572be7a07a517bc10b20
Andrew Cole <andrew.cole@illallangi.com>
2022-03-03 13:32:55 +1100

    Added paginations support

--- publicholidays.ps1
+++ publicholidays.ps1
@@ -7,20 +7,20 @@ function Get-PublicHolidays {
         [string]
         $ResourceID = '33673aca-0857-42e5-b8f0-9981b4755686'
     )
-    $Holidays = @()
-    $URI = "https://data.gov.au/data/api/3/action/datastore_search?resource_id="
-    $Results = (Invoke-RestMethod -Uri "$($URI)$($ResourceID)" -Method Get).Result
-    $JurisdictionResults = $Results.records | Where-Object {($_.Jurisdiction -eq $Jurisdiction)}
-    $holidays = foreach ($Holiday in $JurisdictionResults){
-        $HolidayDate = ([datetime]::ParseExact($Holiday.Date, 'yyyyMMdd', $null)).toshortdatestring()
-        $HolidayEndDate = ([datetime]::ParseExact($Holiday.Date, 'yyyyMMdd', $null))
-        $HolidayEndDateString = ($HolidayEndDate.AddDays(1)).ToShortDateString()
-        [PSCustomObject]@{
-            StartDate = $HolidayDate
-            EndDate = $HolidayEndDateString
-            Name = $Holiday.'Holiday Name'
+    $URI = "/api/3/action/datastore_search?resource_id=$($ResourceID)"
+    do {
+        $Results = (Invoke-RestMethod -Uri "https://data.gov.au$($URI)" -Method Get).Result
+        $JurisdictionResults = $Results.records # | Where-Object {($_.Jurisdiction -eq $Jurisdiction)}
+        foreach ($Holiday in $JurisdictionResults){
+            $HolidayDate = ([datetime]::ParseExact($Holiday.Date, 'yyyyMMdd', $null)).toshortdatestring()
+            $HolidayEndDate = ([datetime]::ParseExact($Holiday.Date, 'yyyyMMdd', $null))
+            $HolidayEndDateString = ($HolidayEndDate.AddDays(1)).ToShortDateString()
+            [PSCustomObject]@{
+                StartDate = $HolidayDate
+                EndDate = $HolidayEndDateString
+                Name = $Holiday.'Holiday Name'
+            }
         }
-
-    }
-    return $Holidays
+        $URI = $Results._links.next
+    } while ($Results.records.length -gt 0)
 }
